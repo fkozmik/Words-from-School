9d1317f3a975e584bc771207ccd9b2f5d7e68855
fkozmik <florian.kozmik@enalean.com>
2026-04-23 15:01:39 +0200

    feat: Pin and use NodeJS 25 from unstable

--- shell.nix
+++ shell.nix
@@ -1,13 +1,19 @@
 { pkgs ? import <nixpkgs> {} }:
 
-pkgs.mkShell {
-  buildInputs = with pkgs; [
-    nodejs_24
-  ];
-  
-  shellHook = ''
-    echo "Environnement prêt pour Mission Spatiale !"
-    echo "Dev :"
-    echo "Lance: npm install && npm run dev"
-  '';
+let
+	nodejs25 = pkgs.nodejs.overrideAttrs rec {
+		version = "25.9.0";
+		src = pkgs.fetchurl {
+		  url = "https://nodejs.org/dist/v${version}/node-v${version}.tar.xz";
+		  hash = "sha256-j3ivPuVfsnhmi1+AHbWL0aOOoWExjrXOISjdvJzYE6o=";
+		};
+	};
+in
+	pkgs.mkShell {
+	  buildInputs = [ nodejs25 ];
+
+	  shellHook = ''
+		echo "Environnement prêt !"
+		echo "Lance: npm install && npm run dev"
+	  '';
 }
