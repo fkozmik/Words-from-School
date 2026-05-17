9ef309925b73c95668a154e9f6959b176e74045b
fkozmik <florian.kozmik@gmail.com>
2026-05-17 11:55:18 +0200

    chore: Update dependencies and rollback to Node LTS

--- shell.nix
+++ shell.nix
@@ -1,19 +1,10 @@
 { pkgs ? import <nixpkgs> {} }:
 
-let
-	nodejs25 = pkgs.nodejs.overrideAttrs rec {
-		version = "25.9.0";
-		src = pkgs.fetchurl {
-		  url = "https://nodejs.org/dist/v${version}/node-v${version}.tar.xz";
-		  hash = "sha256-j3ivPuVfsnhmi1+AHbWL0aOOoWExjrXOISjdvJzYE6o=";
-		};
-	};
-in
-	pkgs.mkShell {
-	  buildInputs = [ nodejs25 ];
+pkgs.mkShell {
+  buildInputs = [ pkgs.nodejs_22 ];
 
-	  shellHook = ''
-		echo "Environnement prêt !"
-		echo "Lance: npm install && npm run dev"
-	  '';
+  shellHook = ''
+	echo "Environnement prêt !"
+	echo "Lance: npm install && npm run dev"
+  '';
 }
